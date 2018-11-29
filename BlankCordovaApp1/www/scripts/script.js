91ac694b9fdc43e358329af00e928a39faf90f70
Vio Cesar Melano Yonanta <44838671+viocesar24@users.noreply.github.com>
2018-11-29 15:47:40 +0700

    Initial commit

---
+++ BlankCordovaApp1/www/scripts/script.js
@@ -0,0 +1,12 @@
+﻿function validasiLogin() {
+    var uname = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    if (uname == "player" && pass == "player") {
+        location.href = "#page-four";
+        //} else if (uname == "admin" && pass == "admin") {
+        //    alert("Selamat datang ADMIN");
+        //    location.href = "#admin-page";
+    } else {
+        document.getElementById("alert-login").innerHTML = "Username dan Password salah";
+    }
+}
