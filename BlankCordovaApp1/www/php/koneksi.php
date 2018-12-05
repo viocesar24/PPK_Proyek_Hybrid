510b98d3f33e403aa1fab9ee480601f471fda796
Wahyu <wahyu.darkflame@gmail.com>
2018-12-05 10:43:57 +0700

    a

--- BlankCordovaApp1/www/php/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$server="localhost";
-	$username="id8114182_funstudy";
-	$password="funstudy";
-	$database="id8114182_projek_ppk";
-	
-	$conn=mysqli_connect($server,$username,$password,$database) or die ('GAGAL');
---
+++ BlankCordovaApp1/www/scripts/navigation.js
@@ -0,0 +1,46 @@
+﻿function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// device APIs are available
+//
+function onDeviceReady() {
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    $('.exit-btn').click(function () {
+        if (confirm('Do you want to exit the app?') == true) {
+            navigator.app.exitApp();
+        }
+    });
+
+    $('.back-btn').click(function () {
+        navigator.app.backHistory();
+    });
+}
+
+function onPause() {
+    // Handle the pause event
+}
+
+function onResume() {
+    // Handle the resume event
+}
+
+function onMenuKeyDown() {
+    // Handle the menubutton event
+}
+
+function onBackKeyDown() {
+    var active_page = $(":mobile-pagecontainer").pagecontainer("getActivePage");
+    var id = active_page.page().attr('id');
+    if (id === 'halaman-login') {
+        if (confirm('Do you want to exit the app?') == true) {
+            navigator.app.exitApp();
+        }
+    }
+    else {
+        navigator.app.backHistory();
+    }
+}
